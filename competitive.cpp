80e611a99f0999d21dd27b844c08c288a12665f3
Priyanshu Shrivastav <official.mr.convict@gmail.com>
2018-03-24 12:53:14 +0530

    Update competitive.cpp

--- competitive.cpp
+++ competitive.cpp
@@ -3,7 +3,6 @@
  | '_ ` _ \| '__/ __/ _ \| '_ \ \ / / |/ __| __|
  | | | | | | | | (_| (_) | | | \ V /| | (__| |_ 
  |_| |_| |_|_|(_)___\___/|_| |_|\_/ |_|\___|\__|
- 
  When I wrote this, only God and I understood what I was doing
     * Now, God only knows*/
 

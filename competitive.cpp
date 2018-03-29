2d3984ba38783bb68135d9dfdc5d243e3cd8ebe4
Priyanshu Shrivastav <official.mr.convict@gmail.com>
2018-03-29 08:05:12 +0000

    Update competitive.cpp
    added templates

--- competitive.cpp
+++ competitive.cpp
@@ -11,7 +11,6 @@
 #define mp make_pair
 #define se second
 #define fi first
-#define ll long long
 #define fr(x,y,z) for(auto x=y;x!=z;++x)
 #define fo(n) for(ll i=0;i<n;i++)
 #define all(v) v.begin(),v.end()
@@ -22,16 +21,23 @@
 #define eR(x, y, z) cout<<x<<" "<<y<<" "<<z<<endl
 #define y(x); {er("YES",x); return 0;}
 #define n(x); {er("NO",x); return 0;}
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int ui;
+
 using namespace std;
 
-void pV(vector<int>*v){vit;fr(it,v->begin(),v->end())e(*it);e(endl);}
-void pS(set<int>* s){sit;fr(it,s->begin(),s->end())e(*it);e(endl);} 
-int gcd(int a,int b){int t,x,y;x=max(a,b);y=min(a,b);while(y!=0){t=x;x=y;y=t%y;}return x;} 
-int lcm(int a,int b){ return(a*b)/gcd(a,b);}
+template <typename T> void pV(const vector<T> &v) {for(auto &it:v)e(it)<<endl;} 
+template <typename T> void pS(const set<T> &s) {for(auto&it:s)e(it)<<endl;}
+template <typename T> T gcd(T &a, T &b){T t,x,y; x = max(a,b); y=min(a,b); while(y!=0){t=x; x=y; y=t%y;} return x;}
+template <typename T> T lcm(T &a,T &b){return(a*b)/gcd<T>(a,b);}
+
 int start_s=clock();
-void timer(){int stop_s=clock();cout<<"time taken: "<<(stop_s-start_s)/double(CLOCKS_PER_SEC)*1000<<endl;}
+void timer(){int stop_s=clock();er("time taken: ",(stop_s-start_s)/double(CLOCKS_PER_SEC)*1000);}
 
 int main(int argc, char *argv[]){
-	//Beware no shit stuff here!
+//Beware no shit stuff here!
 }
 /*it was hard to write, so it should be hard to read!*/
+
+

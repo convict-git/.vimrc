5e63445299a913aae41021f4b9dc46bc8bf59124
Priyanshu Shrivastav <official.mr.convict@gmail.com>
2018-04-05 22:56:40 +0530

    Create cp.cpp

---
+++ cp.cpp
@@ -0,0 +1,34 @@
+/*author* Priyanshu Shrivastav
+ *_ __ ___  _ __ ___ ___  _ ____   ___  ___| |_ 
+ | '_ ` _ \| '__/ __/ _ \| '_ \ \ / / |/ __| __|
+ | | | | | | | | (_| (_) | | | \ V /| | (__| |_ 
+ |_| |_| |_|_|(_)___\___/|_| |_|\_/ |_|\___|\__|
+ When I wrote this, only God and I understood what I was doing
+    * Now, only God knows*/
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define se second
+#define fi first
+#define endl "\n"
+#define fr(x,y,z) for(auto x=y;x!=z;++x)
+#define fo(n) for(auto i=0;i<n;i++)
+#define fm(m,n) fr(i,0,m) fr(j,0,n)
+#define all(v) v.begin(),v.end()
+#define vit vector<int>::iterator it
+#define sit set<int>::iterator it
+#define R(x,y); {cout<<x<<"\n"; return 0;}
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int ui;
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+//Beware no shit stuff here!
+}

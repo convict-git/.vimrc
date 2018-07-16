3ef0d7af6f7d6275643150bb512b7b404f8ff226
Priyanshu Shrivastav <official.mr.convict@gmail.com>
2018-07-16 23:32:35 +0530

    Add files via upload

---
+++ templates/temp.c
@@ -0,0 +1,18 @@
+/*author: Priyanshu Shrivastav (mr.convict)
+ * No copyrights associated. Readers are free to use it everywhere, any where.
+ * If you find bugs, please ping me on official[dot]mr[dot]convict[at]gmail[dot]com */
+
+/*TOPIC:
+ * stack
+*/
+/*DISCLAIMER:*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+int main (void) {
+	/*Your code goes here...*/
+	return 0;
+}

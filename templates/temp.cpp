a94fd2a65b720b9d9fc77169a3d5190ab839c00e
Priyanshu Shrivastav <official.mr.convict@gmail.com>
2018-08-15 21:26:07 +0530

    Update temp.cpp

--- templates/temp.cpp
+++ templates/temp.cpp
@@ -8,6 +8,7 @@
  * Now, only God knows*/
 
 #include <bits/stdc++.h>
+#include <ctime>
 #define pb push_back
 #define eb emplace_back
 #define mp make_pair
@@ -17,7 +18,8 @@
 #define rall(v) (v).rbegin(), (v).rend()
 #define sqr(x) ((x)*(x))
 #define mod(x) ((x)%N)
-#define INF 1000000000
+#define INF (N-7)
+#define EPS (long double)1e-9
 
 using namespace std;
 
@@ -30,24 +32,15 @@ typedef vector<ii> vii;
 
 const ui N = 1000000007;
 
-int main(int argc, char *argv[]){
+int main() {
 #ifdef CONVICTION
 	FILE* fp_in = freopen ("in", "r", stdin);
-	FILE* fp_err = freopen ("err", "w", stderr);
-	int start_s=clock();
-	cerr.precision (3); cerr << fixed;
 #endif
 	ios_base::sync_with_stdio(false);
 	cin.tie (NULL); cout.precision (10); cout << fixed;
 
 	/*solution starts here...*/
 
-#ifdef CONVICTION
-	cerr << "\ntime taken: " <<
-		((int)clock()-start_s)/double(CLOCKS_PER_SEC)*1000 << " s\n";
-#endif
-
-//Beware no shit stuff here!
 	return 0;
 }
 

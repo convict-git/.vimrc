5519e9525b1e2f2db5ae964bc4b3140326a3a91b
Priyanshu Shrivastav <official.mr.convict@gmail.com>
2018-08-18 14:34:27 +0530

    Update temp.cpp

--- templates/temp.cpp
+++ templates/temp.cpp
@@ -1,14 +1,16 @@
-/*author* Priyanshu Shrivastav
-*from*(IIT Palakkad)
- *_ __ ___  _ ______ ___  _ ____   ___  ___| |_
- | '_ ` _ \| '__/ __/ _ \| '_ \ \ / / |/ __| __|
- | | | | | | | | (_| (_) | | | \ V /| | (__| |_
- |_| |_| |_|_|(_)___\___/|_| |_|\_/ |_|\___|\__|
- When I wrote this, only God and I understood what I was doing
- * Now, only God knows*/
+/*author* Priyanshu Shrivastav (from IIT Palakkad) *
+ * *_ __ ___  _ ______ ___  _ ____   ___  ___| |_  *
+ * | '_ ` _ \| '__/ __/ _ \| '_ \ \ / / |/ __| __| *
+ * | | | | | | | | (_| (_) | | | \ V /| | (__| |_  *
+ * |_| |_| |_|_|(_)___\___/|_| |_|\_/ |_|\___|\__| *
+When I wrote this, only God and I understood what I was doing
+ ** * * * * * * * Now, only God knows * * * * * * */
 
 #include <bits/stdc++.h>
-#include <ctime>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
 #define pb push_back
 #define eb emplace_back
 #define mp make_pair
@@ -16,31 +18,26 @@
 #define fi first
 #define all(v) (v).begin(),(v).end()
 #define rall(v) (v).rbegin(), (v).rend()
-#define sqr(x) ((x)*(x))
-#define mod(x) ((x)%N)
+#define SQR(x) ((x)*(x))
+#define MOD(x) ((x)%N)
 #define INF (N-7)
-#define EPS (long double)1e-9
+#define EPS ((ld)1e-9)
 
-using namespace std;
-
+typedef long double ld;
 typedef long long ll;
 typedef unsigned long long ull;
 typedef unsigned int ui;
-typedef vector<int> vi;
-typedef pair<int, int> ii;
-typedef vector<ii> vii;
 
-const ui N = 1000000007;
+const ui N = (ui)1e9 + 7;
 
 int main() {
 #ifdef CONVICTION
-	FILE* fp_in = freopen ("in", "r", stdin);
+	 if(!freopen ("in", "r", stdin)) return -1;
 #endif
-	ios_base::sync_with_stdio(false);
-	cin.tie (NULL); cout.precision (10); cout << fixed;
+	 ios_base::sync_with_stdio(false);
+	 cin.tie (NULL); cout.precision (10); cout << fixed;
 
-	/*solution starts here...*/
+	 /*solution starts here...*/
 
-	return 0;
+	 return 0;
 }
-

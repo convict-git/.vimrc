3fb5ba58500cc290b8e828b74d696b61578ebd7f
Priyanshu Shrivastav <official.mr.convict@gmail.com>
2018-10-13 12:02:32 +0530

    Update temp.cpp

--- templates/temp.cpp
+++ templates/temp.cpp
@@ -1,4 +1,4 @@
-/* author: Priyanshu Shrivastav, from IIT Palakkad *
+/*author* Priyanshu Shrivastav (from IIT Palakkad) *
  * *_ __ ___  _ ______ ___  _ ____   ___  ___| |_  *
  * | '_ ` _ \| '__/ __/ _ \| '_ \ \ / / |/ __| __| *
  * | | | | | | | | (_| (_) | | | \ V /| | (__| |_  *
@@ -6,40 +6,38 @@
 When I wrote this, only God and I understood what I was doing
  ** * * * * * * * Now, only God knows * * * * * * */
 
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <string>
+#include <climits>
 using namespace std;
-using namespace __gnu_pbds;
 
-#define pb push_back
-#define eb emplace_back
-#define mp make_pair
-#define se second
-#define fi first
-#define all(v) (v).begin(),(v).end()
-#define rall(v) (v).rbegin(), (v).rend()
-#define SQR(x) ((x)*(x))
-#define MOD(x) ((x)%_mod)
-#define INF (_mod-7)
-#define EPS ((ld)1e-9)
-#define MAXN ((int)1e5+10)
+#define pb      push_back
+#define eb      emplace_back
+#define se      second
+#define fi      first
+#define all(v) 	(v).begin(),(v).end()
+#define sqr(x) 	((x)*(x))
+#define Mod(x) 	((x)%_mod)
+#define inf     (0x7fffffff)
+#define eps     ((double)1e-9)
 
-typedef long double ld;
-typedef long long ll;
-typedef unsigned long long ull;
-typedef unsigned int ui;
+typedef int64_t ll;
+typedef uint64_t ull;
 
-const ui _mod = (ui)1e9 + 7;
+const int maxn = (int)2e6 + 10;
+const int _mod = (int)1e9 + 7;
 
 int main() {
 #ifdef CONVICTION
-	 if(!freopen ("in", "r", stdin)) return 26;
+  if(!freopen ("in", "r", stdin))return -1;
 #endif
-	 ios_base::sync_with_stdio(false);
-	 cin.tie (NULL); cout.precision (10); cout << fixed;
+  //ios_base::sync_with_stdio(false);
+  //cin.tie (NULL); cout.precision (10); cout << fixed;
+  /*solution starts here...*/
 
-	 /*solution starts here...*/
-
-	 return 0;
+  return 0;
 }
+

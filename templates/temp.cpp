b46cf35faba5de7a416d34e408d5eedf770222e5
Priyanshu Shrivastav <official.mr.convict@gmail.com>
2018-11-01 10:35:14 +0530

    Update temp.cpp

--- templates/temp.cpp
+++ templates/temp.cpp
@@ -5,41 +5,40 @@
  * |_| |_| |_|_|(_)___\___/|_| |_|\_/ |_|\___|\__| *
 When I wrote this, only God and I understood what I was doing
  ** * * * * * * * Now, only God knows * * * * * * */
-
-#include <iostream>
-#include <algorithm>
-#include <cstdlib>
-#include <cmath>
-#include <vector>
-#include <set>
-#include <string>
-#include <climits>
-using namespace std;
-
+#include         <iostream>
+#include         <algorithm>
+#include         <functional>
+#include         <cstdlib>
+#include         <cmath>
+#include         <vector>
+#include         <set>
+#include         <string>
+#include         <climits>
+#include         <map>
 #define pb       push_back
 #define eb       emplace_back
 #define se       second
 #define fi       first
 #define all(v)   (v).begin(),(v).end()
-#define sqr(x)   ((x)*(x))
-#define Mod(x)   ((x)%_mod)
-#define bug(s,x) fprintf(stderr,"%s %s : %i\n",s,#x,x)
+#define bug(s,x) fprintf(stderr,"[%s %s : %i]\n",s,#x,x)
+#define bg(x)    bug ("here : ", x)
 #define inf      (0x7fffffff)
 #define eps      ((double)1e-9)
-
+#define scanf    warnscanf_shit = scanf
+#define IOS      ios_base::sync_with_stdio(false)
+#define PREC     cin.tie (NULL); cout.precision (10); cout << fixed;
+using namespace std;
+int warnscanf_shit;
 typedef int64_t ll;
 typedef uint64_t ull;
-
-const int maxn = (int)2e6 + 10;
-const int _mod = (int)1e9 + 7;
+const int maxn = 1e5 + 10;
+const int _mod = 1e9 + 7;
 
 int main() {
+	 //IOS; PREC;
 #ifdef CONVICTION
-  if(!freopen ("in", "r", stdin)) return -1;
+	 if(!freopen ("in", "r", stdin)) return EXIT_FAILURE;
 #endif
-  //ios_base::sync_with_stdio(false);
-  //cin.tie (NULL); cout.precision (10); cout << fixed;
-  /*solution starts here...*/
 
-  return 0;
+	 return EXIT_SUCCESS;
 }

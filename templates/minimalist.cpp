5bd114a79cc8f5c91e82ec6c98afb95f2d52419d
Priyanshu Shrivastav <official.mr.convict@gmail.com>
2018-12-25 12:06:40 +0530

    Create minimalist.cpp

---
+++ templates/minimalist.cpp
@@ -0,0 +1,22 @@
+/*author* Priyanshu Shrivastav (from IIT Palakkad) *
+ * *_ __ ___  _ ______ ___  _ ____   ___  ___| |_  *
+ * | '_ ` _ \| '__/ __/ _ \| '_ \ \ / / |/ __| __| *
+ * | | | | | | | | (_| (_) | | | \ V /| | (__| |_  *
+ * |_| |_| |_|_|(_)___\___/|_| |_|\_/ |_|\___|\__| *
+When I wrote this, only God and I understood what I was doing
+ ** * * * * * * * Now, only God knows * * * * * * */
+ 
+#pragma GCC      optimize ("Ofast")
+#include         <bits/stdc++.h>
+#define IOS      ios_base::sync_with_stdio(false); cin.tie (nullptr)
+#define PREC     cout.precision (10); cout << fixed
+using namespace std;
+
+int main() {
+	IOS; PREC;
+
+
+	return EXIT_SUCCESS;
+}
+
+
